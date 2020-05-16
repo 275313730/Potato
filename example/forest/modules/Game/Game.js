cd8e753416de79d0fd8785faa4577b536a1ddf5c
275313730 <a275313730@qq.com>
2020-05-16 15:45:37 +0800

    修改Game属性

--- example/forest/modules/Game/Game.js
+++ example/forest/modules/Game/Game.js
@@ -5,28 +5,54 @@ import { unit } from "./Unit.js";
 export class Game {
     // 初始化Game类
     static init(options) {
-        // 获取canvas
-        this.canvas = document.getElementById(options.el)
-        // canvas上下文
-        this.context = this.canvas.getContext('2d')
-        // 宽度
-        this.width = options.width
-        // 高度
-        this.height = options.height
-        // 帧数
-        this.frames = 60
-        // 动画间隔帧(每隔n帧绘制下一个关键帧)
-        this.animationInterval = 16
-        // 键盘状态
-        this.key = null
-        // 鼠标状态
-        this.mouseDown = false
-        // 测试(显示精灵外框)
-        this.test = false
-        // 图片路径
-        this.imagePath = options.path ? options.path.image : ''
-        // 音频路径
-        this.audioPath = options.path ? options.path.audio : ''
+        Object.defineProperties(this, {
+            // canvas
+            canvas: {
+                value: document.getElementById(options.el)
+            },
+            // canvas上下文
+            context: {
+                value: this.canvas.getContext('2d')
+            },
+            // 宽度
+            width: {
+                value: options.width
+            },
+            // 高度
+            height: {
+                value: options.height
+            },
+            // 动画间隔帧(每隔n帧绘制下一个关键帧)
+            animationInterval: {
+                value: options.animationInterval || 16,
+                writable: true
+            },
+            // 键盘状态
+            key: {
+                value: null,
+                writable: true
+            },
+            // 鼠标状态
+            mouseDown: {
+                value: false,
+                writable: true
+            },
+            // 测试(显示精灵外框)
+            test: {
+                value: false,
+                writable: true
+            },
+            // 图片路径
+            imagePath: {
+                value: options.path ? options.path.image : '',
+                writable: true
+                // 音频路径
+            },
+            audioPath: {
+                value: options.path ? options.path.audio : '',
+                writable: true
+            }
+        })
 
         // 初始化实例方法
         this.asset = asset(this.imagePath, this.audioPath)
