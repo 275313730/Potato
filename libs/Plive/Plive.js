644d8de867f0b6efc8dc8bfec01c7132ad7926c5
275313730 <a275313730@qq.com>
2020-05-18 19:39:39 +0800

    修改Pox库

--- libs/Plive/Plive.js
+++ libs/Plive/Plive.js
@@ -1,15 +1,23 @@
 export function plive() {
     let stageData = {}
+    let lastStage = null
     return function () {
         this.plive = {
             save(name, data) {
                 stageData[name] = data
+                lastStage = name
             },
             load(name) {
                 return stageData[name]
             },
+            loadLast() {
+                return stageData[lastStage]
+            },
             clear(name) {
                 delete stageData[name]
+            },
+            clearAll() {
+                stageData = {}
             }
         }
     }
