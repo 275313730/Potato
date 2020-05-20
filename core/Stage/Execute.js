d3776d6ab2f2ce6cdcf006ea998ee772fd5eeb42
275313730 <a275313730@qq.com>
2020-05-20 10:44:30 +0800

    完善Stage和Unit的生命周期函数

--- core/Stage/Execute.js
+++ core/Stage/Execute.js
@@ -5,6 +5,9 @@ export function execute(stage) {
 
     // 单位渲染和事件
     function unitExecute(unit, camera) {
+        // 更新前函数
+        unit.beforeUpdate && unit.beforeUpdate()
+
         // 计算相对位置
         unit.relX = unit.x - camera.x * (1 - unit.fixed)
         unit.relY = unit.y - camera.y * (1 - unit.fixed)
@@ -17,11 +20,17 @@ export function execute(stage) {
 
         // 执行事件
         unit.event.execute()
+
+        // 更新后函数
+        unit.updated && unit.updated()
     }
 
     // 场景刷新
     function refresh() {
         if (stop) { return }
+
+        // 更新前函数
+        stage.beforeUpdate && stage.beforeUpdate()
 
         // 清除canvas
         Game.context.clearRect(0, 0, Game.width, Game.height)
@@ -37,12 +46,16 @@ export function execute(stage) {
             unitExecute(unit, camera)
         })
 
+        // 更新后函数
+        stage.updated && stage.updated()
+
         window.requestAnimationFrame(refresh)
     }
 
     return {
         // 销毁
         destory() {
+            // 销毁前函数
             stage.beforeDestroy && stage.beforeDestroy()
 
             // 退出循环
@@ -51,6 +64,7 @@ export function execute(stage) {
             // 清空场景精灵
             Game.unit.delAll()
 
+            // 销毁后函数
             stage.destoryed && stage.destoryed()
         },
         // 进入循环
