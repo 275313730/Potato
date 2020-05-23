0f9675c828c8068f9ef1b5ccffece94b7e9f63f0
275313730 <a275313730@qq.com>
2020-05-23 16:27:05 +0800

    修改框架代码，版本升级至v0.4.0

--- core/Sprite/Graphics.js
+++ core/Sprite/Graphics.js
@@ -2,10 +2,6 @@ import { Game } from "../Game/Game.js"
 
 export function graphics(unit) {
     // 声明常量
-    const GAME_WIDTH = Game.width
-    const GAME_HEIGHT = Game.height
-
-    // 声明函数
     const ctx = Game.context
     const floor = Math.floor
 
@@ -110,7 +106,7 @@ export function graphics(unit) {
 
 
             // 内部属性
-            let intervalCount = 0,
+            let currInterval = 0,
                 currFrame = 0
 
             // 动画属性
@@ -147,13 +143,13 @@ export function graphics(unit) {
                 // 绘制动画
                 drawAnimation(animation.image, options.flip, currFrame)
 
-                // 间隔帧计数
-                intervalCount++
+                // 动画间隔帧增加
+                currInterval++
 
                 // 判断计数是否小于间隔帧数
-                if (intervalCount >= options.animationInterval) {
+                if (currInterval >= options.animationInterval) {
                     // 动画当前间隔帧归零
-                    intervalCount = 0
+                    currInterval = 0
 
                     // 动画关键帧增加
                     currFrame++
@@ -269,12 +265,17 @@ export function graphics(unit) {
         },
         // 渲染
         render() {
-            const { relX, relY, drawWidth, drawHeight, scale } = getData()
-            if (relX + drawWidth * scale < 0 || relX > GAME_WIDTH ||
-                relY + drawHeight * scale < 0 || relY > GAME_HEIGHT) {
+            const gw = Game.width
+            const gh = Game.height
+            const ux = unit.relX
+            const uy = unit.relY
+            const uw = unit.width
+            const uh = unit.height
+            const scale = unit.scale
+            if (ux + uw * scale < 0 || ux > gw || uy + uh * scale < 0 || uy > gh) {
                 return
             }
-            ctx.globalAlpha = unit.alpha
+            Game.context.globalAlpha = unit.alpha
             executor && executor()
             Game.test && test()
         },
