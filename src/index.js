e80fba0f46427da242b9acbe10db3fd8a75bf06f
275313730 <a275313730@qq.com>
2020-08-22 22:25:19 +0800

    去掉不必要的console

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import Game from "./Game/index.js";
 import Stage from "./Stage/index.js";
 import Sprite from "./Sprite/index.js";
 
-console.log("Potato | version:0.9.1 | author:Roanne");
-
 export { Game, Stage, Sprite }
