c478ca1a0f2fd37961158bf38f692610f92b51dd
Roanne <a275313730@qq.com>
2023-05-22 23:35:44 +0800

    更新

--- Demo.ts
+++ Demo.ts
@@ -1,11 +1,9 @@
-import Game, { Sprites, Components, Enums } from "./src/Index";
+import Game, { Sprites, Components, Enums } from "./src/index";
 
 (async () => {
   Game.generate("potato")
   Game.resolution = { x: 1920, y: 1080 }
 
-  const spineSprite = new Sprites.SpineSprite("char_249_mlyss", "Idle", "skel")
-  spineSprite.scale = { x: 2, y: 2 }
 
   //Game.camera.followSprite(girl, Enums.AnchorPoint.CENTER)
 })()
